df16c1988a7c2a28520c166d77adb326e605e151
Ahmyd Yasser <ahmed1amr33@gmail.com>
2025-05-11 18:31:59 +0300

    po

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Compile Java files
-javac -cp "lib/*" -d target/classes src/main/java/*.java
 
+# Clean and build the project using Maven
+mvn clean package
 
-# Run the app
-java -cp "target/classes:lib/*" LoginGUI
+# Check if build was successful
+if [ $? -eq 0 ]; then
+    echo "Build successful! You can run the application using:"
+    echo "java -jar target/bug-tracker-1.0-SNAPSHOT.jar"
+else
+    echo "Build failed!"
+    exit 1
+fi
